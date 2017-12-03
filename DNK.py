4d2a2b63f45dd8b92cb397bb2b94f39f227e3f0f
Svetik <31988884+Svetik3d@users.noreply.github.com>
2017-12-03 20:30:44 +0400

    Переделалалалалала....

--- DNK.py
+++ DNK.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # -*-coding: utf-8 -*-
 # vim: sw=4 ts=4 expandtab ai
-#Задания олимпиадные по программированию  https-acmp.ru №278
+#ЗАДАЧА №278 	Вычислительная биология
 
-import json
-import requests
-import datetime
-import os
+with open("/home/sveta/dnk.txt", "r") as files:
+	r = files.read().split("\n")
+	s = r[0]
+	print s
+	t = r[1]
+	print t
 
-s = raw_input("Скажите последовательность - ")
-t = raw_input("Скажите строку - ")
-aki = 0
 n = 0
-ot = []
 
 for b in t:
+	print n
+	print b
+	print s[n]
 	if s[n] == b:
 		n = n+1
-		ot.append("YES")
-if len(s) == len(ot):
-	print "YES"
-else:
-	print "NO"
+		if n == len(s):
+			break
+
+with open("/home/sveta/dnk.txt", "w") as filess:
+	if len(s) == n:
+		filess.write("YES")
+	else:
+		filess.write("NO")
+
