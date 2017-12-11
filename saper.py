04959ca3f14b6e33bbf3834dc0fbe699ddf085a0
Svetik <31988884+Svetik3d@users.noreply.github.com>
2017-12-11 22:46:44 +0400

    Исправила ошибку в 3 тесте, теперь ошибка в 4)))

--- saper.py
+++ saper.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*-coding: utf-8 -*-
 # vim: sw=4 ts=4 expandtab ai
 
-
 li=[]
 
-with open("/home/sveta/saperproga.txt", "r") as files:
+with open("INPUT.txt", "r") as files:
 	r = files.read().split("\n")
 	per_str = r[0].split(" ")
 	n = int(per_str[0])
 	m = int(per_str[1])
 	k = int(per_str[2])
 	for i in range(1,1+n):
-		listr = ["0"]*m
+		listr = ["."]*m
 		li.append(listr)
 	for noms in range(1,k+1):
 		strs = r[noms].split(" ")
@@ -21,9 +20,11 @@ with open("/home/sveta/saperproga.txt", "r") as files:
 		li[sr][st] = "*"
 
 def plus(l,ab,am,x,y):
-	if l[ab][am] == "*" and l[x][y] != "*":
-		if x>-1 and y>-1:
-			l [x][y]= str(int(l[x][y])+1)
+	if x>=0 and y>=0 and x<len(l) and y < len(l) :
+		if l[ab][am] == "*" and l[x][y] != "*":
+			if l[x][y] == ".":
+				l[x][y] = 0
+			l[x][y]= str(int(l[x][y])+1)
 
 for akb in range(0, len(li)):
 	for akm in range(0, len(li[akb])):
@@ -44,9 +45,6 @@ for akb in range(0, len(li)):
 		#СверхуСправа
 		plus(li,akb,akm,akb-1,akm+1)
 
-
-
-for i in range(len(li)):
-	print "".join(li[i])
-
-
+with open("OUTPUT.txt", "w") as filess:
+	for i in li:
+		filess.write("".join(i)+"\n")
